c2e58c16cd0db2df44d25f5dc3c3dd9b984c1e41
zyb <elseif@live.cn>
2024-07-16 15:45:42 +0800

    modified:   chr.sh

--- chr.sh
+++ chr.sh
@@ -1,12 +1,13 @@
 dd if=/dev/zero of=/dev/sda  bs=1 count=512
-sgdisk --clear --set-alignment=2 \
+sgdisk --set-alignment=2 \
     --new=1::+32M --typecode=1:8300 --change-name=1:"RouterOS Boot" --attributes=1:set:2 \
     --new=2::-0 --typecode=2:8300 --change-name=2:"RouterOS" \
     --gpttombr=1:2 \
     /dev/sda
 dd if=/dev/sda of=pt.bin bs=1 count=66 skip=446
 echo -e "\x80" | dd of=pt.bin  bs=1 count=1  conv=notrunc
-sgdisk --mbrtogpt --clear --set-alignment=2 \
+dd if=/dev/zero of=/dev/sda  bs=1 count=512
+sgdisk --set-alignment=2 \
     --new=1::+32M --typecode=1:8300 --change-name=1:"RouterOS Boot" --attributes=1:set:2 \
     --new=2::-0 --typecode=2:8300 --change-name=2:"RouterOS" \
     /dev/sda
