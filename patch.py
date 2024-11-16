d0301e5b7ee8b64fd10b2830047097f8dc562e6c
AsifRahamanZihad <42118322+AsifRahamanZihad@users.noreply.github.com>
2024-11-16 21:29:00 +0600

    Update patch.py
    Signed-off-by: AsifRahamanZihad <42118322+AsifRahamanZihad@users.noreply.github.com>

--- patch.py
+++ patch.py
@@ -293,7 +293,7 @@ def patch_npk_package(package,key_dict):
         patch_squashfs(extract_dir,key_dict)
         logo = os.path.join(extract_dir,"nova/lib/console/logo.txt")
         run_shell_command(f"sudo sed -i '1d' {logo}") 
-        run_shell_command(f"sudo sed -i '8s#.*#  elseif@live.cn     https://github.com/elseif/MikroTikPatch#' {logo}")
+        run_shell_command(f"sudo sed -i '8s#.*#  Mikrotik     https://mikrotik.com#' {logo}")
         print(f"pack {extract_dir} ...")
         run_shell_command(f"rm -f {squashfs_file}")
         run_shell_command(f"mksquashfs {extract_dir} {squashfs_file} -quiet -comp xz -no-xattrs -b 256k")
