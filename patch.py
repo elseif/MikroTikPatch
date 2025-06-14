50a2f3f03bf4ce79a3e9a1d0ab58059133020731
elseif <elseif@live.cn>
2025-06-14 20:19:25 +0800

    Update patch.py
    Signed-off-by: elseif <elseif@live.cn>

--- patch.py
+++ patch.py
@@ -24,6 +24,8 @@ def replace_key(old,new,data,name=''):
     data =  replace_chunks(old_chunks, new_chunks, data,name)
     
     if 'ARCH' in os.environ and  os.environ['ARCH'] == '-arm64':
+        old_chunks = [old[i:i+4] for i in range(0, len(old), 4)]
+        new_chunks = [new[i:i+4] for i in range(0, len(new), 4)]
         old_bytes = old_chunks[4] + old_chunks[5] + old_chunks[2] + old_chunks[0] + old_chunks[1] + old_chunks[6] + old_chunks[7]
         new_bytes = new_chunks[4] + new_chunks[5] + new_chunks[2] + new_chunks[0] + new_chunks[1] + new_chunks[6] + new_chunks[7]
         if old_bytes in data:
