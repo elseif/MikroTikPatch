a350e2badd29f2354b1200528755f3aa9c683539
elseif <elseif@live.cn>
2025-06-18 18:24:26 +0800

    Update patch.py
    Signed-off-by: elseif <elseif@live.cn>

--- patch.py
+++ patch.py
@@ -294,11 +294,12 @@ def patch_loader(loader_file):
         
 def patch_squashfs(path,key_dict):
     for root, dirs, files in os.walk(path):
-        for file in files:
-            if file =='loader':
-                patch_loader(loader_file)
-            file = os.path.join(root,file)
+        for _file in files:
+            file = os.path.join(root,_file)
             if os.path.isfile(file):
+                if _file =='loader':
+                    patch_loader(file)
+                    continue
                 data = open(file,'rb').read()
                 for old_public_key,new_public_key in key_dict.items():
                     _data = replace_key(old_public_key,new_public_key,data,file)
