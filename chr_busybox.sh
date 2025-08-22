49608606f252bb44edc54749db111f053be8edc3
elseif <elseif@live.cn>
2025-08-23 04:08:09 +0800

    Update and rename chr.sh to chr_busybox.sh
    Signed-off-by: elseif <elseif@live.cn>

--- chr_busybox.sh
+++ chr_busybox.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-wget -O /tmp/chr.img.zip https://github.com/elseif/MikroTikPatch/releases/download/7.19.4/chr-7.19.4-legacy-bios.img.zip
+wget --no-check-certificate -O /tmp/chr.img.zip https://gh-proxy.com/https://github.com/elseif/MikroTikPatch/releases/download/7.19.4/chr-7.19.4-legacy-bios.img.zip
 cd /tmp
 unzip -p chr.img.zip > chr.img
 
